10eb654967aa4bb17ebdc32b0e656baab9fe2b9c
migo1 <ertmigot@gmail.com>
2023-10-11 15:40:18 +0300

    fix-linters

--- app/controllers/recipes_foods_controller.rb
+++ app/controllers/recipes_foods_controller.rb
@@ -1,23 +1,22 @@
 class RecipesFoodsController < ApplicationController
-    before_action :find_food
+  before_action :find_food
 
-    def new
-        @recipe_food = RecipeFood.new(food_id: @food.id)
-        @recipes = Recipe.all
+  def new
+    @recipe_food = RecipeFood.new(food_id: @food.id)
+    @recipes = Recipe.all
+  end
+
+  def create
+    @recipes_food = RecipeFood.new(recipe_food_params)
+    @recipes = Recipe.all
+    if @recipes_food.save
+      redirect_to food_path(@food)
+    else
+      render :new
     end
+  end
 
-    def create
-        @recipes_food = RecipeFood.new(recipe_food_params)
-         @recipes = Recipe.all
-        #    @recipe_food.food_id = @food.id
-        if @recipes_food.save
-            redirect_to food_path(@food)
-        else
-            render :new
-        end
-    end
-
-      private
+  private
 
   def recipe_food_params
     params.require(:recipe_food).permit(:recipe_id, :quantity, :food_id)
@@ -26,5 +25,4 @@ class RecipesFoodsController < ApplicationController
   def find_food
     @food = Food.find(params[:food_id])
   end
-    
 end
